7a358be4fc23db5afbf582b220e97859caf0c7cf
Artur Przybysz <prz.artur@gmail.com>
2019-03-23 16:26:01 +0100

    work in progress

--- src/main/java/main/App.java
+++ src/main/java/main/App.java
@@ -1,16 +1,7 @@
 package main;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
-    public static String siema() {
-        return "siema";
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
     }
 }
