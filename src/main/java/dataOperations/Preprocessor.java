e3426a850008f63146b8021b9336e1eb726d54f7
Artur Przybysz <prz.artur@gmail.com>
2019-03-31 11:00:13 +0200

    stoplist, lemmization

---
+++ src/main/java/dataOperations/Preprocessor.java
@@ -0,0 +1,66 @@
+package dataOperations;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Properties;
+
+import data.DataTables;
+import edu.stanford.nlp.ling.CoreAnnotations;
+import edu.stanford.nlp.ling.CoreLabel;
+import edu.stanford.nlp.pipeline.Annotation;
+import edu.stanford.nlp.pipeline.StanfordCoreNLP;
+import edu.stanford.nlp.util.CoreMap;
+
+public class Preprocessor {
+    private StanfordCoreNLP pipeline;
+
+    public Preprocessor() {
+        Properties props;
+        props = new Properties();
+        props.put("annotators", "tokenize, ssplit, pos, lemma");
+        this.pipeline = new StanfordCoreNLP(props);
+    }
+
+    public String cleanse(String text) {
+        text = text.toLowerCase();
+        text = text.replaceAll("[^\\S ]+g", " "); // white chars to spaces
+        text = text.replaceAll("[^a-z., ]", ""); // remove non a-z . , and spaces
+        text = text.replaceAll("  +", " "); // multiple spaces to single
+        text = text.replaceAll("( (a-z) )", ""); // single characters surrounded by spaces
+
+        return text;
+    }
+
+    public List<String> applyStopList(List<String> words) {
+        List<String> stopList = DataTables.getStopList();
+        List<String> prunedWords = new ArrayList<>();
+
+        for (String word : words) {
+            if (!stopList.contains(word.toLowerCase())) {
+                prunedWords.add(word);
+            }
+        }
+        return prunedWords;
+    }
+
+    public List<String> lemme(String text) {
+        List<String> lemmas = new LinkedList<>();
+
+        Annotation document = new Annotation(text);
+
+        this.pipeline.annotate(document);
+
+        List<CoreMap> sentences = document.get(CoreAnnotations.SentencesAnnotation.class);
+        for (CoreMap sentence : sentences) {
+            for (CoreLabel token : sentence.get(CoreAnnotations.TokensAnnotation.class)) {
+                lemmas.add(token.get(CoreAnnotations.LemmaAnnotation.class));
+            }
+        }
+        return lemmas;
+    }
+
+    public String[] stem(String[] textWords) {
+        return textWords;
+    }
+}
--- src/main/java/main/App.java
+++ src/main/java/main/App.java
@@ -2,9 +2,10 @@ package main;
 
 import classifiedObjects.Article;
 import config.Config;
+import dataOperations.TextStatistics;
 import dataOperations.dataLoading.DataLoader;
 import dataOperations.dataLoading.fileReaders.ReutersArticleReader;
-import dataOperations.Preprocessing;
+import dataOperations.Preprocessor;
 
 import java.util.List;
 
@@ -12,13 +13,20 @@ public class App {
     public static void main(String[] args) {
         ReutersArticleReader reader = new ReutersArticleReader();
         DataLoader<Article> loader = new DataLoader<>();
+        Preprocessor preprocessor = new Preprocessor();
 
         List<Article> articles = loader.loadObjects(Config.learningSetPath, reader);
         Article a = articles.get(0);
 
         System.out.println(a.getText());
-        System.out.println(Preprocessing.cleanse(a.getText()));
+        List<String> words = preprocessor.lemme(a.getText());
+        System.out.println(words);
 
-        System.out.println(articles.size());
+        List<String> prunedWords = preprocessor.applyStopList(words);
+
+        System.out.println(prunedWords);
+        System.out.println(TextStatistics.wordOccurrences(words));
+        System.out.println(TextStatistics.wordOccurrences(prunedWords));
+
     }
 }
